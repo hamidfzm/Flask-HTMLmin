b7aca5fb4ba1586ea13fae9e60c576c70475bb60
Hamid FzM <hamidfzm@gmail.com>
2016-01-14 13:25:07 +0330

    bug fix; some html pages break while trying to minify them

--- flask_htmlmin.py
+++ flask_htmlmin.py
@@ -1,10 +1,9 @@
-__author__ = 'Hamid FzM'
-
 from htmlmin.main import minify
+
+__author__ = 'Hamid FzM'
 
 
 class HTMLMIN(object):
-
     def __init__(self, app=None):
         self.app = app
         if app is not None:
@@ -16,14 +15,15 @@ class HTMLMIN(object):
         if app.config['MINIFY_PAGE']:
             app.after_request(self.response_minify)
 
-    def response_minify(self, response):
+    @staticmethod
+    def response_minify(response):
         """
         minify response html to decrease traffic
         """
         if response.content_type == u'text/html; charset=utf-8':
             response.set_data(
                 minify(response.get_data(as_text=True),
-                       remove_comments=True)
+                       remove_comments=True, reduce_empty_attributes=True, remove_optional_attribute_quotes=False)
             )
 
             return response
