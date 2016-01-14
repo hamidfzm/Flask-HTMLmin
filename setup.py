a6fcfe20170187b7e89ba3961ea81aeb8952db39
Hamid FzM <hamidfzm@gmail.com>
2016-01-14 13:26:25 +0330

    update package version to 1.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup
 
 setup(
     name='Flask-HTMLmin',
-    version='1.0',
+    version='1.1',
     url='https://github.com/hamidfzm/Flask-HTMLmin',
     license='BSD',
     author='Hamid FzM',
