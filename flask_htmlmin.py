8427142065ddb8e974494b26baa7f0469c754f35
Runar Trollet Kristoffersen <runartrollet@me.com>
2017-09-21 18:59:16 +0200

    Add useroptions for minify

--- flask_htmlmin.py
+++ flask_htmlmin.py
@@ -1,22 +1,33 @@
+"""Flask-HTMLmin."""
+
 from htmlmin.main import minify
 
 __author__ = 'Hamid FzM'
 
 
 class HTMLMIN(object):
-    def __init__(self, app=None):
+    """Minify flask text/html mime types responses."""
+
+    def __init__(self, app=None, options=None):
         self.app = app
+        self.options = {
+            'remove_comments': True,
+            'reduce_empty_attributes': True,
+            'remove_optional_attribute_quotes': False
+            }
+        if options:
+            self.options.update(options)
         if app is not None:
             self.init_app(app)
 
     def init_app(self, app):
+        """Initialize onto app."""
         app.config.setdefault('MINIFY_PAGE', False)
 
         if app.config['MINIFY_PAGE']:
             app.after_request(self.response_minify)
 
-    @staticmethod
-    def response_minify(response):
+    def response_minify(self, response):
         """
         minify response html to decrease traffic
         """
@@ -24,7 +35,7 @@ class HTMLMIN(object):
             response.direct_passthrough = False
             response.set_data(
                 minify(response.get_data(as_text=True),
-                       remove_comments=True, reduce_empty_attributes=True, remove_optional_attribute_quotes=False)
+                       **self.options)
             )
 
             return response
