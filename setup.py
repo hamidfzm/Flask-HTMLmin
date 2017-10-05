193e3a4cac560c3e2de2ad4cd64d030198304e49
Hamid FzM <hamidfzm@gmail.com>
2017-10-05 09:30:14 +0330

    Update setup.py version to 1.3.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup
 
 setup(
     name='Flask-HTMLmin',
-    version='1.2.1',
+    version='1.3.0',
     url='https://github.com/hamidfzm/Flask-HTMLmin',
     license='BSD',
     author='Hamid FzM',
