d1369bdd9589321496f0cc6cf2de2052f9658def
Hamid FzM <hamidfzm@gmail.com>
2018-10-20 03:23:12 +0330

    Implement Route (or URL rule) exemption with test

--- test.py
+++ test.py
@@ -1,19 +1,20 @@
-from os import close, unlink
-from tempfile import mkstemp
-from pytest import fixture
-from flask import Flask, jsonify
-from flask_htmlmin import HTMLMIN
 from json import loads
 
+from flask import Flask, jsonify
+from pytest import fixture
+
+from flask_htmlmin import HTMLMIN
+
 app = Flask(__name__)
+app.config['TESTING'] = True
+app.config['MINIFY_PAGE'] = True
+htmlmin = HTMLMIN(app=app)
 
 json_resp = dict(
     resp='some unminifed json response'
 )
 
-@app.route('/')
-def html():
-    return '''<html>
+html_resp = '''<html>
             <body>
                 <h1>
                     HTML
@@ -21,15 +22,25 @@ def html():
             </body>
         </html>'''
 
+
+@app.route('/')
+def html():
+    return html_resp
+
+
 @app.route('/json')
 def json():
     return jsonify(json_resp)
 
+
+@app.route('/exempt')
+@htmlmin.exempt
+def exempt():
+    return html_resp
+
+
 @fixture
 def client():
-    app.config['TESTING'] = True
-    app.config['MINIFY_PAGE'] = True
-    HTMLMIN(app=app)
     client = app.test_client()
     yield client
 
@@ -39,7 +50,15 @@ def test_html_minify(client):
     resp = client.get('/').data
     assert b'<html> <body> <h1> HTML </h1> </body> </html>' == resp
 
+
 def test_json_unminifed(client):
     """ testing unminifed Json response """
     resp = client.get('/json').data
     assert json_resp == loads(resp)
+
+
+def test_exempt_routes(client):
+    """ testing exempt routes """
+    resp = client.get('/exempt').data
+
+    assert resp == html_resp.encode()
