7b205d29d74438dc58dbb976a8017fe9aec5faaf
Matthew Wisdom <matthewwisdom11@gmail.com>
2021-10-07 23:38:22 +0100

    fix test

--- test.py
+++ test.py
@@ -15,8 +15,13 @@ json_resp = dict(
 )
 
 html_resp = '''<html>
+            <style>
+             .h {
+               width: 3em
+              } 
+           </style>
             <body>
-                <h1>
+                <h1 style="width: 3em;;">
                     HTML
                 </h1>
             </body>
@@ -48,7 +53,8 @@ def client():
 def test_html_minify(client):
     """ testing HTML minified response """
     resp = client.get('/').data
-    assert b'<html> <body> <h1> HTML </h1> </body> </html>' == resp
+    print(resp)
+    assert b'<html> <style>.h{width:3em}</style><body> <h1 style="width:3em"> HTML </h1> </body> </html>' == resp
 
 
 def test_json_unminifed(client):
