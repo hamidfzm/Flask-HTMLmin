9c5afd1f9f4b930e9b8a62581ef28ed5713f7a36
Matthew Wisdom <matthewwisdom11@gmail.com>
2021-10-17 20:24:16 +0100

    Fixed linting issues

--- test.py
+++ test.py
@@ -18,7 +18,7 @@ html_resp = '''<html>
             <style>
              .h {
                width: 3em
-              } 
+              }
            </style>
             <body>
                 <h1 style="width: 3em;;">
@@ -54,7 +54,8 @@ def test_html_minify(client):
     """ testing HTML minified response """
     resp = client.get('/').data
     print(resp)
-    assert b'<html> <style>.h{width:3em}</style><body> <h1 style="width:3em"> HTML </h1> </body> </html>' == resp
+    assert b'<html> <style>.h{width:3em}</style><body>\
+ <h1 style="width:3em"> HTML </h1> </body> </html>' == resp
 
 
 def test_json_unminifed(client):
